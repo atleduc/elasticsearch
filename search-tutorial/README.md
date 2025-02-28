3ae7a72676327e1d13ab9f9d5d673ad54764dc3d
aleduc <arnaud.leduc@keyconsulting.fr>
2025-02-28 23:59:32 +0100

    initial commit

---
+++ search-tutorial/README.md
@@ -0,0 +1,4 @@
+# Elasticsearch Search Tutorial
+
+This directory contains a starter Flask project used in the Search tutorial.
+https://www.elastic.co/search-labs/tutorials/search-tutorial/full-text-search/search-basics
---
+++ search-tutorial/search.py
@@ -0,0 +1,61 @@
+import json
+from pprint import pprint
+import os
+import time
+
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
+
+load_dotenv()
+
+
+class Search:
+    def __init__(self):
+        self.es = Elasticsearch('http://localhost:9200')  # <-- connection options need to be added here
+        client_info = self.es.info()
+        print('Connected to Elasticsearch!')
+        pprint(client_info.body)
+
+    def create_index(self):
+        self.es.indices.delete(index='my_documents', ignore_unavailable=True)
+        self.es.indices.create(index='my_documents')
+        
+    def insert_document(self, document):
+        return self.es.index(index='my_documents', body=document)
+
+    def insert_documents(self, documents):
+        operations = []
+        for document in documents:
+            operations.append({'index': {'_index': 'my_documents'}})
+            operations.append(document)
+        return self.es.bulk(operations=operations)
+
+    def reindex(self):
+        self.create_index()
+        with open('data.json', 'rt') as f:
+            documents = json.loads(f.read())
+        return self.insert_documents(documents)
+
+    def search(self, **query_args):
+        return self.es.search(index='my_documents', **query_args)
+
+    def retrieve_document(self, id):
+        return self.es.get(index='my_documents', id=id)
+
+    def extract_filters(query):
+        filters = []
+
+        filter_regex = r'category:([^\s]+)\s*'
+        m = re.search(filter_regex, query)
+        if m:
+            filters.append({
+                'term': {
+                    'category.keyword': {
+                        'value': m.group(1)
+                    }
+                }
+            })
+            query = re.sub(filter_regex, '', query).strip()
+
+        return {'filter': filters}, query
+    
